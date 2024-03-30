22a432046e53586f8b6cd4ed5648c9c6f996e4f1
ChurroC <charanvenkatchandran@gmail.com>
2024-03-29 21:51:35 -0500

    whyyyyyyy auth issues

--- review-script.js
+++ review-script.js
@@ -5,17 +5,17 @@
     // const review = document.querySelector('form[id=diary-entry-form]>fieldset>div.form-row:nth-of-type(2)');
     // review.innerHTML = '<strong>Public Review:</strong>' + review.innerHTML;
 
-    const privateReview = review.cloneNode(true);
-    const movieTitle =
-        document.querySelector('.film-title-wrapper>a')?.innerText ??
-        document.querySelector('.headline-1.js-widont.prettify')?.innerText;
+    // const privateReview = review.cloneNode(true);
+    // const movieTitle =
+    //     document.querySelector('.film-title-wrapper>a')?.innerText ??
+    //     document.querySelector('.headline-1.js-widont.prettify')?.innerText;
 
-    privateReview.querySelector('textarea').value = (await chrome.storage.sync.get([movieTitle]))[movieTitle] ?? '';
-    privateReview.querySelector('textarea').id = 'private-review';
-    privateReview.querySelector('strong').innerHTML =
-        'Private Review: <span style="font-size: x-small">(You only have 1 private review in total which cannot be deleted and remember to click the submit button to save it)</span>';
+    // privateReview.querySelector('textarea').value = (await chrome.storage.sync.get([movieTitle]))[movieTitle] ?? '';
+    // privateReview.querySelector('textarea').id = 'private-review';
+    // privateReview.querySelector('strong').innerHTML =
+    //     'Private Review: <span style="font-size: x-small">(You only have 1 private review in total which cannot be deleted and remember to click the submit button to save it)</span>';
 
-    review.insertAdjacentElement('afterend', privateReview);
+    // review.insertAdjacentElement('afterend', privateReview);
 
     // const submit = document.querySelector('#diary-entry-submit-button');
     // submit.addEventListener('click', () => {
@@ -27,7 +27,7 @@
     modal.querySelector('.form-row.clearfix').remove();
     modal.id = 'private-review-modal';
 
-    const privateReview = review.cloneNode(true);
+    const privateReview = modal.querySelector('textarea');
     const movieTitle =
         document.querySelector('.film-title-wrapper>a')?.innerText ??
         document.querySelector('.headline-1.js-widont.prettify')?.innerText;
